5aee39fc9175faa38c50b2f30f0b9aa0fa0536ab
Jure Grabnar <jgrabnar@biosistemika.com>
2019-05-14 09:02:04 +0200

    Update CreateFilePreviewService with open3 and content types

--- app/services/create_file_preview_service.rb
+++ app/services/create_file_preview_service.rb
@@ -2,40 +2,40 @@
 
 class CreateFilePreviewService
   extend Service
+  require 'open3'
 
-  CONTENT_TYPES = [
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', # .docx
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation', # .pptx
-    'application/vnd.ms-powerpoint', # .ppt
-  ].freeze
+  attr_reader :errors, :output_file_path
 
-  attr_reader :errors
-
-  def initialize(input_file)
-    # TODO: check content type, extend to  multiple files
-    @input_file = input_file
+  def initialize(input_file_path)
+    @input_file_path = input_file_path
     @errors = {}
   end
 
   def libreoffice_path
-    'libreoffice'
+    ENV['LIBREOFFICE_PATH'] || 'libreoffice'
   end
 
   def call
-    directory = File.dirname(@input_file.path)
-    basename  = File.basename(@input_file.path, '.*')
-    png_file  = File.join(directory, "#{basename}.png")
+    directory = File.dirname(@input_file_path)
+    basename  = File.basename(@input_file_path, '.*')
+    @output_file_path = File.join(directory, "#{basename}.png")
 
-    system libreoffice_path, '--headless',
-      '--invisible', '--convert-to', 'png',
+    cmd = [
+      libreoffice_path,
+      '--headless', '--invisible',
+      '--convert-to', 'png',
       '--outdir', directory,
-      @input_file.path
+      @input_file_path
+    ]
 
+    Rails.logger.debug("Running: #{cmd.join(' ')}")
+    stdout_and_stderr, status = Open3.capture2e(*cmd)
+    Rails.logger.debug("Stdout/stderr message: #{stdout_and_stderr}, status: #{status}.")
     self
-    # Paperclip.io_adapters.for(Asset.find(35).file)
-    # CreateFilePreviewService.new(f).call
+  rescue StandardError => e
+    @errors[e.class.to_s.downcase.to_sym] = e.message
+    self
   end
-
 
   def succeed?
     @errors.none?
